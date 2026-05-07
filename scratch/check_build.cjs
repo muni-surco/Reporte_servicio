82f06b08fad3723c9698bbc86e0c724d3b0072b2
munisurco <carlos.macutela@munisurco.gob.pe>
2026-05-07 09:22:32 -0500

    remove temps

--- scratch/check_build.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('c:\\Users\\USER\\Documents\\GitHub\\Reporte_servicio\\dist\\index.html', 'utf8');
-const search = 'Tc=';
-const idx = content.indexOf(search);
-if (idx >= 0) {
-    console.log(content.substring(idx - 100, idx + 400));
-} else {
-    console.log('Not found');
-}
